c1c5d11de76c22fb5ed0183e651a0b7000f43a0d
John Worthley <john@Johns-MacBook-Pro-2.local>
2018-05-28 22:26:10 -0400

    add build

--- App.js
+++ App.js
@@ -18,3 +18,10 @@ export default class App extends Component {
     );
   }
 }
+process.env.NODE_ENV = 'production'
+var reactScriptsConfig = require('react-scripts/config/webpack.config.prod')
+module.exports = Object.assign({}, reactScriptsConfig, {
+  output: Object.assign({}, reactScriptsConfig.output, {
+     path: './dist'
+  })
+})
